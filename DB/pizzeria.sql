67a23a7b877017505b030997dcffce3408ed3a42
QVandenhove <quentin.vandenhove@viacesi.fr>
2020-10-15 09:23:44 +0200

    MAJ DB

--- DB/pizzeria.sql
+++ DB/pizzeria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 14 oct. 2020 à 14:19
+-- Généré le : jeu. 15 oct. 2020 à 09:22
 -- Version du serveur :  10.4.11-MariaDB
 -- Version de PHP : 7.4.5
 
@@ -20,8 +20,26 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `pizzeria`
 --
-CREATE DATABASE IF NOT EXISTS `pizzeria` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `pizzeria`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+CREATE TABLE IF NOT EXISTS `commande` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `montant` float NOT NULL,
+  `estPaye` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `commande`
+--
+
+INSERT INTO `commande` (`id`, `montant`, `estPaye`) VALUES
+(2, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -29,28 +47,47 @@ USE `pizzeria`;
 -- Structure de la table `pizza`
 --
 
-CREATE TABLE `pizza` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `pizza` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) DEFAULT NULL,
-  `prix` double DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `prix` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `pizza`
 --
 
 INSERT INTO `pizza` (`id`, `nom`, `prix`) VALUES
-(0, 'Reine', 5.5);
+(1, 'Reine', 6),
+(2, 'Calzone', 8.99),
+(4, 'Potez', 798.56),
+(5, 'Déchet', 1);
+
+-- --------------------------------------------------------
 
 --
--- Index pour les tables déchargées
+-- Structure de la table `pizzacommandee`
+--
+
+CREATE TABLE IF NOT EXISTS `pizzacommandee` (
+  `idCommande` int(11) NOT NULL,
+  `idPizza` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  KEY `idPizzaFK` (`idPizza`),
+  KEY `idCommandeFK` (`idCommande`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Index pour la table `pizza`
+-- Contraintes pour la table `pizzacommandee`
 --
-ALTER TABLE `pizza`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `pizzacommandee`
+  ADD CONSTRAINT `idCommandeFK` FOREIGN KEY (`idCommande`) REFERENCES `commande` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `idPizzaFK` FOREIGN KEY (`idPizza`) REFERENCES `pizza` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
